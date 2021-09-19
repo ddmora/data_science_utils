c21367a4a67423ec68660c5798b6310e47cc54b3
David Mora Guerrero <ddmora@uc.cl>
2021-09-19 13:06:30 -0600

    Cambios en preprocessing.py para mejorar su operatividad

--- preprocessing/one_hot_encoding.py
+++ preprocessing/one_hot_encoding.py
@@ -16,14 +16,14 @@ class OneHotEncoder:
     Parameters
     ----------
     max_cat: int, default=None
-        The maximun number of categories per column
+        The maximum number of categories per column
     n_min: int, default=2
-        The minimun number of observations to be classified as a category
+        The minimum number of observations to be classified as a category
     load_if_exists: bool, default=False
         If True, Loads the dictionary of categories if is already in the ''. This functionality speed up the time of
         encoding, by loading the existing dictionary of categories. Mainly useful when handling large datasets.
-    drop: bool, default=True
-        If True, drops the original columns.
+    drop_na: bool, default=True
+        If True, drops the missing .
     verbose: bool, default False
         If True, print the report of encoding process.
 
@@ -36,14 +36,14 @@ class OneHotEncoder:
     new_columns: list
         List of names of the new columns created by the one hot encoding
     info_dict: dict
-        A dictionary with column names as entries, and the respecitive observations during the process if exist.
+        A dictionary with column names as entries, and the respective observations during the process if exist.
 
 
     """
 
     # Class attributes
 
-    def __init__(self, max_cat=None, n_min=2, load_if_exists=False, drop=True, drop_last=True, verbose=False,
+    def __init__(self, max_cat=None, n_min=1, load_if_exists=False, drop_last=True, drop_na=True, verbose=False,
                  filename="dict_nominal_columns.pickle"):
 
         # Instance attributes
@@ -51,9 +51,9 @@ class OneHotEncoder:
         self.max_cat = max_cat
         self.n_min = n_min
         self.load_if_exists = load_if_exists
-        self.drop = drop
         self.verbose = verbose
         self.drop_last = drop_last
+        self.drop_na = drop_na
         self.filename = filename
         self.categories = {}
         self.to_encode = []
@@ -69,68 +69,60 @@ class OneHotEncoder:
         else:
             for column in data.columns:
                 if data[column].dtype == object:
-                    counts = data[column].value_counts().sort_values(ascending=False)
+                    counts = data[column].value_counts(dropna=self.drop_na).sort_values(ascending=False)
                     counts = counts[counts >= self.n_min]
-                    categories = [str(x) for x in counts.index]
+                    categories = counts.index.tolist()
 
                     if self.max_cat is None:
                         self.categories.update({column: categories})
 
+                    else:
                         self.categories.update({column: categories[:self.max_cat]})
-                        self.info_dict.update({column: 'exceeds the maximum limit of categories'})
+                        msg = f"{self.max_cat} selected from {len(categories)} categories."
+                        self.info_dict.update({column: msg})
+
             pickle.dump(self.info_dict, open(self.filename, 'wb'))
 
         self.to_encode = list(self.categories.keys())
 
         return self
 
-    def transform(self, data):
+    def transform(self, data, drop_remain=True):
 
-        count = 0
-        for column in self.to_encode:
+        for count, column in enumerate(self.to_encode, start=1):
             if column in data.columns:
-
                 unique_values_column = self.categories[column]
-                # We delete the dummy associated with None if present in unique values.
-                if "nan" in unique_values_column:
-                    unique_values_column = [x for x in unique_values_column if x != "nan"]
-                # ELse we omitted one level
+
                 if self.drop_last & (column not in self.info_dict):
                     unique_values_column = unique_values_column[:-1]
 
-                count += 1
-                to = len(self.to_encode)
-                n_cat = len(unique_values_column)
+                msg_01 = f"Encoding: {column} ..."
+                msg_02 = f"Encoding column number {count} from {len(self.to_encode)} original columns."
+                msg_03 = f"Number of unique values: {len(unique_values_column)}"
 
-                msg_01 = "Encoding: {} ...".format(column)
-                msg_02 = "Encoding column number {} from {} original columns.".format(count, to)
-                msg_03 = "Number of unique values: {}".format(n_cat)
+                for n_val, val in enumerate(unique_values_column, start=1):
 
-                count_2 = 0
-
-                for val in unique_values_column:
-
-                    count_2 += 1
+                    msg_04 = f"Encoding category number {n_val} from {len(unique_values_column)}."
+                    name_temp = column + "_" + str(val)
+                    self.new_columns.append(name_temp)
+                    msg_05 = f"Total of new columns created {len(self.new_columns)}."
 
                     print_if(self.verbose, msg_01)
                     print_if(self.verbose, msg_02)
                     print_if(self.verbose, msg_03)
-
-                    msg_04 = "Encoding category number {} from {}".format(count_2, n_cat)
                     print_if(self.verbose, msg_04)
-
-                    name_temp = column + "_" + str(val)
-                    self.new_columns.append(name_temp)
-                    new = len(self.new_columns)
-                    msg_05 = "Total of new columns created {}.".format(new)
                     print_if(self.verbose, msg_05)
-
-                    data[name_temp] = (data[column] == str(val)) * 1.0
 
                     if self.verbose:
                         clear_output(wait=True)
 
-        if self.drop:
-            data = data.drop(self.to_encode, axis=1)
+                    data[name_temp] = (data[column] == val) * 1.0
+
+                msg_05 = f"Total of new columns created {len(self.new_columns)}."
+                print_if(self.verbose, msg_05)
+
+        if drop_remain:
+            rest = [x for x in data.columns if x not in self.new_columns]
+            data = data.drop(rest, axis=1)
 
         return data
